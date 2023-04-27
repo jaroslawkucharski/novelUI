8ce19d8245ab4af329008eb26b0386e620236893
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2023-04-27 19:31:29 +0200

    jest config updated

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,12 @@
 // eslint-disable-next-line no-undef
 module.exports = {
 	collectCoverage: true,
-	collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+	collectCoverageFrom: [
+		'src/**/*.{ts,tsx}',
+		'!src/**/index.ts',
+		'!src/**/*.d.ts',
+		'!src/**/*.stories.tsx'
+	],
 	coverageDirectory: 'coverage',
 	preset: 'ts-jest',
 	testEnvironment: 'jsdom'
