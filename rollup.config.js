8d3ddb23ff6b48ae825c89198f959a7f93e487e4
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2023-04-27 21:16:13 +0200

    import issue fixed

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 
-import packageJson from './package.json'
+import packageJson from './package.json' assert { type: 'json' }
 
 export default {
 	input: 'src/index.ts',
