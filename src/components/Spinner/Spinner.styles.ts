21befdfd6fb1c69320a751aca8a0a82ac385bf22
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2023-05-02 13:11:12 +0200

    Spinner updated

--- src/components/Spinner/Spinner.styles.ts
+++ src/components/Spinner/Spinner.styles.ts
@@ -2,13 +2,17 @@ import styled, { css } from 'styled-components'
 
 import { SpinnerProps } from './Spinner'
 
+export const SpinnerComponentWrapper = styled.span<SpinnerProps>`
+	cursor: wait;
+	position: absolute;
+	width: 100%;
+	height: 100%;
+`
+
 export const SpinnerComponent = styled.span<SpinnerProps>`
 	${({ theme: { colors }, color }) =>
 		css`
-			cursor: wait;
-			pointer-events: none;
-
-			&::after {
+			&::before {
 				content: '';
 				top: 0;
 				bottom: 0;
@@ -21,15 +25,47 @@ export const SpinnerComponent = styled.span<SpinnerProps>`
 				border-radius: 50%;
 				border-top-color: ${color || colors.secondary100};
 				position: absolute;
-				animation: spinner 1s ease infinite;
+				animation: spinner-top 1s ease infinite;
 			}
 
-			@keyframes spinner {
+			&::after {
+				content: '';
+				top: 0;
+				bottom: 0;
+				margin: auto;
+				left: 0;
+				right: 0;
+				width: 16px;
+				height: 16px;
+				border: 4px solid transparent;
+				border-radius: 50%;
+				border-bottom-color: ${color || colors.secondary100};
+				border-right-color: ${color || colors.secondary100};
+				opacity: 0.5;
+				position: absolute;
+				animation: spinner-bottom 1s ease-out infinite;
+			}
+
+			@keyframes spinner-top {
 				from {
 					transform: rotate(0turn);
 				}
 
 				to {
+					transform: rotate(2turn);
+				}
+			}
+
+			@keyframes spinner-bottom {
+				0% {
+					transform: rotate(0turn);
+				}
+
+				50% {
+					transform: rotate(2turn);
+				}
+
+				100% {
 					transform: rotate(1turn);
 				}
 			}
